99f2096dfd58bc3d77aa4e63395bb970bb6646e6
Alexandr Sinev <sir.sineow@yandex.ru>
2023-12-24 21:01:55 +0300

    add titled_str and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_str(string):
+    """Переводит все буквы в верхний регистр"""
     return string.upper()
+
+def titled_str(string1):
+    """Переводит первые буквы в верхний регистр"""
+
+    return string1.title()
